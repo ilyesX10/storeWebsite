5eb97abfb21f1c6a415761c0442c55dfbca68a61
ilyesX10 <ilyesdrouiche00@gmail.com>
2026-09-04 20:00:51 +0100

    14

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -6,7 +6,9 @@ import { supabase } from '../lib/supabaseClient';
 
 function Browse() {
     const [books, setBooks] = useState([]);
+    const [loading,setLoading] = useState(false)
     async function fetchBooks() {
+        setLoading(true)
         const { data, error } = await supabase
             .from('books')
             .select('*');
@@ -16,6 +18,7 @@ function Browse() {
             console.log('Books from Supabase:', data);
             setBooks(data);
         }
+        setLoading(false)
     }
     useEffect(() => {
         fetchBooks();
@@ -24,12 +27,16 @@ function Browse() {
         <>
             <main className='container mx-auto pt-28 pb-16 px-4 max-w-6xl flex flex-col gap-8 min-h-[80vh]'>
                 <Search />
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                   {
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 place-items-center">
+                    {loading ? (
+                        <div className='size-12 col-span-full'>
+                            <div className='size-full border-t-[3px] border-l-[3px] border-[var(--primary)] rounded-full animate-spin' />
+                        </div>
+                    ) : (
                         books.map((book) => (
                             <BooksCard key={book.id} book={book} />
                         ))
-                   }
+                    )}
                 </div>
             </main>
             <Footer />
